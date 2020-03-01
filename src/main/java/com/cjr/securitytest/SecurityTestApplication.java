8fe66dd7a6281cd3e5254a07176d020590249fb0
cjr <1282654205@qq.com>
2020-03-01 12:01:35 +0800

    1、整合redis和spring-session

--- src/main/java/com/cjr/securitytest/SecurityTestApplication.java
+++ src/main/java/com/cjr/securitytest/SecurityTestApplication.java
@@ -4,9 +4,12 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
 //可以参考 https://gitee.com/hong_ej/SpringSecurity/blob/master/pom.xml
 @SpringBootApplication
 @MapperScan("com.cjr.securitytest.web.security.dal.dao")		// 指定mapper.java 的位置
+@EnableRedisHttpSession     // 整合redis和spring-session
 public class SecurityTestApplication {
 
 	public static void main(String[] args) {
